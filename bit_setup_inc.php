7aa6e00ccfa3703c229e93e8d7c59a860b65a107
wjames5 <will@tekimaki.com>
2010-02-12 14:53:29 +0000

    flatten services hash and introduce new param for adding some meta data about services to easy site configuration, see lcconfig

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -11,12 +11,19 @@ $gBitSystem->registerPackage( $registerHash );
 if( $gBitSystem->isPackageActive( 'geo' ) ) {
 	require_once( GEO_PKG_PATH.'LibertyGeo.php' );
 
-	$gLibertySystem->registerService( LIBERTY_SERVICE_GEO, GEO_PKG_NAME, array(
-		'content_load_sql_function' => 'geo_content_load_sql',
-		'content_list_sql_function' => 'geo_content_list_sql',
-		'content_store_function'  => 'geo_content_store',
-		'content_preview_function'  => 'geo_content_preview',
-		'content_expunge_function'  => 'geo_content_expunge',
-	) );
+	$gLibertySystem->registerService( 
+		LIBERTY_SERVICE_GEO, 
+		GEO_PKG_NAME, 
+		array(
+			'content_load_sql_function' => 'geo_content_load_sql',
+			'content_list_sql_function' => 'geo_content_list_sql',
+			'content_store_function'  => 'geo_content_store',
+			'content_preview_function'  => 'geo_content_preview',
+			'content_expunge_function'  => 'geo_content_expunge',
+		),
+		array( 
+			'description' => 'Enables the addition of geographic data to any content.'
+		)
+	);
 }
-?>
+
